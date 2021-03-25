d007128faf9bae04bb9a63c8c5892a086512ba6d
Maxime <maximegouin0@gmail.com>
2021-03-25 09:57:59 +0100

    Updates

---
+++ precache-manifest.a793c71de2bfe149aa78262997906352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e80bc7e302a9c852d2c676dc2c7b552",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c04ac3c54c15ad6103b9",
+    "url": "./static/js/2.75c9990a.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "./static/js/2.75c9990a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd00e729c95799ea0b99",
+    "url": "./static/js/main.db92386e.chunk.js"
+  },
+  {
+    "revision": "d72e17954f33f09c3f6b",
+    "url": "./static/js/runtime-main.97fb920c.js"
+  }
+]);
